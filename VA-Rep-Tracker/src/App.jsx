479d83278d6daa58c1cf9dd8b2283d3ef9cef74f
AbraamIbrahim <abraamibr8@gmail.com>
2026-03-21 20:44:04 -0400

    removed json packages from .gitignore

--- VA-Rep-Tracker/src/App.jsx
+++ VA-Rep-Tracker/src/App.jsx
@@ -1,9 +1,5 @@
 import { getAllBills } from "./api/bills";
 
-//testing getting all bill for current congress and printing it 
-const billsArray = getAllBills()
-console.log(billsArray)
-
 export default function App() {
   return <div>VA Rep Dashboard</div>
 }
