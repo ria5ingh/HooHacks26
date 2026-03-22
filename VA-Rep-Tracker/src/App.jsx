87899ed444835f81c0bf88551d266cefc1526d92
Aaron Alex <aaronkalex@gmail.com>
2026-03-22 00:59:56 -0400

    fixed styling, no computation

--- VA-Rep-Tracker/src/App.jsx
+++ VA-Rep-Tracker/src/App.jsx
@@ -1,10 +1,6 @@
-import { useEffect } from "react";
-import { getCosponLegislation } from "./api/votes";
-import { getSponLegislation } from "./api/votes";
-import { extractNumberTypeMap } from "./api/votes";
-import { fetchSummariesFromMap } from "./api/votes";
+// App.jsx
 import { useMemo, useState } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { generateContent } from "./api/geminiClient";
 import zipToDist from "../data/ziptodist.json";
 import "./styles/app.css";
@@ -84,37 +80,11 @@ function HomePage() {
   );
 }
 
-/** 
 export default function App() {
-  useEffect(() => {
-    getSponLegislation("L000174")
-      .then((arr) => console.log("sponsored:", arr))
-      .catch((err) => console.error("sponsored error:", err));
-
-    getCosponLegislation("L000174")
-      .then((arr) => console.log("cosponsored:", arr))
-      .catch((err) => console.error("cosponsored error:", err));
-  }, []);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/results" element={<ResultsPage />} />
     </Routes>
   );
-}
-**/
-
-export default function App() {
-  useEffect(() => {
-    getSponLegislation("L000174")
-      .then((arr) => {
-        const map = extractNumberTypeMap(arr);
-        return fetchSummariesFromMap(map, "119");
-      })
-      .then((summaries) => console.log(summaries))
-      .catch((err) => console.error(err));
-  }, []);
-
-  return <div>VA Rep Dashboard</div>;
 }
